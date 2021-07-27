bb9a8154406650e1919f1ef8b808d9bb0ff6ec6c
Yuri Ly <mail@lyo.su>
2021-07-28 00:47:45 +0300

    0.1.12 fix error on counter

--- handlers/counter.js
+++ handlers/counter.js
@@ -3,7 +3,7 @@ const Composer = require('telegraf/composer')
 const composer = new Composer()
 
 composer.on('message', Composer.groupChat(async (ctx, next) => {
-  if (ctx.message.reply_to_message && ctx.message.reply_to_message.from.id === 777000) {
+  if (ctx.message.reply_to_message && ctx.message.reply_to_message.forward_from_chat && ctx.message.reply_to_message.from.id === 777000) {
     const channel = await ctx.db.Channel.findOne({ channelId: ctx.message.reply_to_message.forward_from_chat.id })
     const post = await ctx.db.Post.findOne({ channel, channelMessageId: ctx.message.reply_to_message.forward_from_message_id })
     if (!post) return next()
